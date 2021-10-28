1babaadc36fc3c89100fe310ba1fa2599e455b14
vr-greycube <vijay@greycube.in>
2021-10-28 14:38:07 +0530

    fix: payment entry contact_phone from SI/SO contact_mobile

--- amrut_energy/doc_events.py
+++ amrut_energy/doc_events.py
@@ -19,16 +19,9 @@ def on_validate_payment_entry(doc, method):
         if doc.references:
             ref = doc.references[-1]
             reference_doctype = ref.get("reference_doctype")
-            for d in frappe.db.sql("""
-            select
-                coalesce(con.mobile_no,cus.mobile_no,'')
-                from 
-                (select %(party)s party, %(party_type)s party_type) pe
-                left outer join `tab{doctype}` d on d.name = %(reference_name)s
-                left outer join tabContact con on con.name = d.contact_person
-                left outer join tabCustomer cus on cus.name = pe.party and pe.party_type = 'Customer'
-            """.format(doctype=reference_doctype), dict(party = doc.party, party_type = doc.party_type, reference_name = ref.name), debug=True):
-                contact_phone = d[0]
+            contact_phone = frappe.db.get_value(reference_doctype, ref.name,"contact_mobile")
+            if not contact_phone and doc.party_type=="Customer":
+                contact_phone = frappe.db.get_value("Customer", doc.party,"mobile_no")
         if contact_phone:
             doc.contact_phone = contact_phone
 
