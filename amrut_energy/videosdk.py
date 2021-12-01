24caecee18c02f054d460860d98b0ba59a3a0132
vr-greycube <vijay@greycube.in>
2021-12-01 19:38:22 +0530

    feat: add videosdk settings to app settings
    cache videosdk token

--- amrut_energy/videosdk.py
+++ amrut_energy/videosdk.py
@@ -11,54 +11,61 @@ import uuid
 import datetime
 
 
-videosdk_api_url = "https://api.videosdk.live/v1/meetings"
+# videosdk_api_url = "https://api.videosdk.live/v1/meetings"
 
 # https://docs.videosdk.live/docs/realtime-communication/sdk-reference/prebuilt-sdk-js/setup/
 
 
 @frappe.whitelist(allow_guest=True)
-def generate_token():
-    conf = frappe._dict(frappe.conf.videosdk)
-    expires = 24 * 3600
-    now = datetime.datetime.utcnow()
-    exp = now + datetime.timedelta(seconds=expires)
-    token = jwt.encode(
-        payload={"apikey": conf.api_key, "permissions": ["allow_join"]},
-        key=conf.secret_key,
-    ).decode("utf-8")
-
-    print("token: ", token)
-    return token
-
-
-@frappe.whitelist(allow_guest=True)
 def create_meeting(**args):
-    token = generate_token()
+
+    settings = frappe.get_single("Amrut Settings")
+
+    token = get_token()
     headers = {"authorization": f"{token}"}
-    response = requests.request("POST", videosdk_api_url, headers=headers)
+    response = requests.request(
+        "POST", settings.videosdk_api_url + "v1/meetings", headers=headers
+    )
 
     result = response.json()
 
-    conf = frappe._dict(frappe.conf.videosdk)
-
     meeting_url = "{}/call?meeting_id={}&api_key={}&meeting_title={}".format(
-        frappe.utils.get_site_url(frappe.local.site),
+        frappe.utils.get_url(),
         result.get("meetingId"),
-        conf.api_key,
+        settings.videosdk_api_key,
         args.get("subject"),
     )
 
     return meeting_url
 
 
-@frappe.whitelist(allow_guest=True)
-def validate_meeting(meetingId=""):
-    print("*\n" * 25, "meetingId", meetingId)
-    print(frappe.form_dict)
-    token = generate_token()
-    url = videosdk_api_url + "/" + meetingId
-    headers = {"authorization": f"{token}"}
-    response = requests.request("POST", url, headers=headers)
+def get_token():
+    def _generate_token():
+        settings = frappe.get_single("Amrut Settings")
+        expires = 24 * 3600
+        now = datetime.datetime.utcnow()
+        exp = now + datetime.timedelta(seconds=expires)
+        token = jwt.encode(
+            payload={
+                "apikey": settings.videosdk_api_key,
+                "permissions": ["allow_join"],
+                "exp": exp,
+            },
+            key=settings.videosdk_secret_key,
+        ).decode("utf-8")
+        return token
 
-    print(response.json())
-    return response.json()
+    return frappe.cache().get_value("videosdk_token", _generate_token)
+
+
+# @frappe.whitelist(allow_guest=True)
+# def validate_meeting(meetingId=""):
+#     print("*\n" * 25, "meetingId", meetingId)
+#     print(frappe.form_dict)
+#     token = generate_token()
+#     url = videosdk_api_url + "v1/meetings" + meetingId
+#     headers = {"authorization": f"{token}"}
+#     response = requests.request("POST", url, headers=headers)
+
+#     print(response.json())
+#     return response.json()
